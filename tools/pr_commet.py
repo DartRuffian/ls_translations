c1e02d99d44b0d8e055315234735941dbc4b9828
Dart <59131299+DartRuffian@users.noreply.github.com>
2026-08-31 10:11:48 -0500

    Test

---
+++ tools/pr_commet.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Author: DartRuffian
+
+Comments on a GitHub PR for missing translations
+"""
+
+import os
+import sys
+import traceback
+import subprocess as sp
+from github import Github, Auth
+
+
+def update_translations(repo,pr_number):
+    diag = sp.check_output(
+        ["python3", "tools/translation_progress.py", "--markdown"])
+    diag = str(diag, "utf-8")
+    pr = repo.get_pull(pr_number)
+    pr.create_issue_comment(body=diag)
+
+
+def main():
+    print("Obtaining environment variables ...")
+    try:
+        token = os.environ["GITHUB_TOKEN"]
+        owner = os.environ["OWNER"]
+        repo_name = os.environ["REPO"]
+        pr_number = os.environ["PR_NUMBER"]
+        auth = Auth.Token(token)
+        repo = Github(auth=auth).get_repo(f"{owner}/{repo_name}")
+    except:
+        print("Could not obtain vars.")
+        print(traceback.format_exc())
+        return 1
+    else:
+        print("Sucessfully obtained environment variables.")
+
+    print("\nUpdating translation issue ...")
+    try:
+        update_translations(repo,pr_number)
+    except:
+        print("Failed to update translation issue.")
+        print(traceback.format_exc())
+        return 1
+    else:
+        print("Successfull commented on pull request.")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
